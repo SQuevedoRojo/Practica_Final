0c4c9059d0218e89ac2af26d6b4b623a6ccce17a
hp <hp@DESKTOP-928HOVP>
2024-04-11 13:43:09 +0200

    haz pull puta

--- src/Agenda/Principal.java
+++ src/Agenda/Principal.java
@@ -9,5 +9,6 @@ public class Principal
         System.out.println("...");
         System.out.println("quiero que me funcione el pull y no se meta en rebasing");
         System.out.println("Pues no me jodas el repositorio");
+        System.out.println("");
     }//main()
 }//class
