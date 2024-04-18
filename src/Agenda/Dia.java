e7b44acd2df63e05101e9cce312734f46202e98b
hp <hp@DESKTOP-928HOVP>
2024-04-18 14:18:04 +0200

    mod clases

--- src/Agenda/Dia.java
+++ src/Agenda/Dia.java
@@ -1,9 +1,33 @@
 package Agenda;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 /**
  * 
  * @author Asier Sergio
  */
 public class Dia {
+    private ArrayList<Evento>[] eventos = new ArrayList[48];
+
+    public Dia() {
+    }
     
+    public void crearEventoDiaEnt(String nombre, LocalTime tiempo,int dia){
+        int hora=tiempo.getHour(),min=tiempo.getMinute();
+        int posArray=(hora*2)+(min==0?0:1);
+        eventos[posArray].add(new Evento(nombre, tiempo, dia));
+    }
+    //falta por editar instrucciones
+    public void crearRecordatorio(String nombre, LocalTime tiempo,int dia,boolean anual){
+        int hora=tiempo.getHour(),min=tiempo.getMinute();
+        int posArray=(hora*2)+(min==0?0:1);
+        eventos[posArray].add(new Evento(nombre, tiempo, dia));
+    }
+    
+    public void crearTarea(String nombre, LocalTime tiempo,int dia,LocalTime durEsti, boolean urgente){
+        int hora=tiempo.getHour(),min=tiempo.getMinute();
+        int posArray=(hora*2)+(min==0?0:1);
+        eventos[posArray].add(new Evento(nombre, tiempo, dia));
+    }
 }
--- src/Agenda/Recordatorio.java
+++ src/Agenda/Recordatorio.java
@@ -1,9 +1,17 @@
 package Agenda;
 
+import java.time.LocalTime;
+
 /**
  * 
  * @author Asier Sergio
  */
 public class Recordatorio extends Evento{
+    private boolean anual;
+
+    public Recordatorio(boolean anual, String nombre, LocalTime hora, int dia) {
+        super(nombre, hora, dia);
+        this.anual = anual;
+    }
     
 }
