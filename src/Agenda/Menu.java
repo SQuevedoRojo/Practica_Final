95157dca9fb8856220f0f72d122cb04c4bef7e51
ALUMNO <ALUMNO@DESKTOP-282UC07>
2024-04-18 13:57:43 +0200

    MENU

--- src/Agenda/Menu.java
+++ src/Agenda/Menu.java
@@ -30,16 +30,9 @@ public class Menu
             if(opcionPrincipal != 4)
             {
                 switch (opcionPrincipal) {
-                    case 1:
-                        
-                        break;
-                    case 2:
-                        
-                        break;
-                        
-                    case 3:
-                        
-                        break;
+                    case 1:opcionesEventosTareas();break;
+                    case 2:opcionesContactos();break;
+                    case 3:opcionesImprimirDias();break;
                 }
             }
         }while (opcionPrincipal != 4);
@@ -79,18 +72,32 @@ public class Menu
     {
         int op = -1;
         limpiar();
-        System.out.println("1.- Crear un Evento Recordatorio");
-        System.out.println("2.- Crear un Evento Tarea");
-        System.out.println("3.- Borrar un Evento Recordatorio");
-        System.out.println("4.- Borrar un Evento Tarea");
-        System.out.println("5.- Imprimir todos los eventos de un día determinado");
-        System.out.println("6.- Imprimir todos los eventos de un mes determinado");
-        System.out.println("7.- Imprimir un evento por mes, día y hora");
-        System.out.println("8.- Leer de un fichero todos los eventos de un año");
-        System.out.println("9.- Guardar de un fichero de texto todos los eventos de un año");
-        System.out.println("10.- Guardar en un fichero de texto todos los eventos de un mes");
-        System.out.println("11.- Guardar en un fichero de texto todos los eventos de un día");
-        System.out.println("12.- Salir");
+        System.out.println("\t\t1.- Crear un Evento Recordatorio");
+        System.out.println("\t\t2.- Crear un Evento Tarea");
+        System.out.println("\t\t3.- Borrar un Evento Recordatorio");
+        System.out.println("\t\t4.- Borrar un Evento Tarea");
+        System.out.println("\t\t5.- Imprimir todos los eventos de un día determinado");
+        System.out.println("\t\t6.- Imprimir todos los eventos de un mes determinado");
+        System.out.println("\t\t7.- Imprimir un evento por mes, día y hora");
+        System.out.println("\t\t8.- Leer de un fichero todos los eventos de un año");
+        System.out.println("\t\t9.- Guardar de un fichero de texto todos los eventos de un año");
+        System.out.println("\t\t10.- Guardar en un fichero de texto todos los eventos de un mes");
+        System.out.println("\t\t11.- Guardar en un fichero de texto todos los eventos de un día");
+        System.out.println("\t\t12.- Salir");
+        op = comprobarScanner(12);
+        switch (op) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 5: break;
+            case 6: break;
+            case 7: break;
+            case 8: break;
+            case 9: break;
+            case 10: break;
+            case 11: break;
+        }
     }//opcionesEventosTareas()
     
     private void opcionesImprimirDias()
