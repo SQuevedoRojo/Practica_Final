0bf90c7fe9dbe608c00a41a070bbe110fe541ae6
ALUMNO <ALUMNO@DESKTOP-282UC07>
2024-04-18 19:33:59 +0200

    MENU

--- src/Agenda/Menu.java
+++ src/Agenda/Menu.java
@@ -13,8 +13,7 @@ import java.util.Scanner;
 public class Menu 
 {
     private Scanner entrada = new Scanner(System.in);
-    public Menu() {
-    }
+    public Menu() {}
     
     public void opcionesPrincipales() throws AWTException, InterruptedException
     {
@@ -43,14 +42,16 @@ public class Menu
         do{
             try 
             {
-                System.out.print("\n\tElige la opcion correspondiente -> ");
+                System.out.print("\n\t\tElige la opcion correspondiente [1.."+opMax+"] -> ");
                 opcion = entrada.nextInt();
             } 
             catch (InputMismatchException e)
             {
-                System.out.println("\n\tNo has introducido un número entero");
+                System.out.println("\n\t\tNo has introducido un número entero");
                 entrada.nextLine();
             }
+            if (!(opcion >= 1 && opcion <= opMax))
+                System.out.println("\n\t\tIntroduce una opcion en el rango 1.."+opMax);
         }while(!(opcion >= 1 && opcion <= opMax));
         return opcion;
     }//comprobarScanner()
@@ -69,18 +70,18 @@ public class Menu
     {
         int op = -1;
         limpiar();
-        System.out.println("\t\t1.- Crear un Evento Recordatorio");
-        System.out.println("\t\t2.- Crear un Evento Tarea");
-        System.out.println("\t\t3.- Borrar un Evento Recordatorio");
-        System.out.println("\t\t4.- Borrar un Evento Tarea");
-        System.out.println("\t\t5.- Imprimir todos los eventos de un día determinado");
-        System.out.println("\t\t6.- Imprimir todos los eventos de un mes determinado");
-        System.out.println("\t\t7.- Imprimir un evento por mes, día y hora");
-        System.out.println("\t\t8.- Leer de un fichero todos los eventos de un año");
-        System.out.println("\t\t9.- Guardar de un fichero de texto todos los eventos de un año");
-        System.out.println("\t\t10.- Guardar en un fichero de texto todos los eventos de un mes");
-        System.out.println("\t\t11.- Guardar en un fichero de texto todos los eventos de un día");
-        System.out.println("\t\t12.- Salir");
+        System.out.println("\n\t1.- Crear un Evento Recordatorio");
+        System.out.println("\t2.- Crear un Evento Tarea");
+        System.out.println("\t3.- Borrar un Evento Recordatorio");
+        System.out.println("\t4.- Borrar un Evento Tarea");
+        System.out.println("\t5.- Imprimir todos los eventos de un día determinado");
+        System.out.println("\t6.- Imprimir todos los eventos de un mes determinado");
+        System.out.println("\t7.- Imprimir un evento por mes, día y hora");
+        System.out.println("\t8.- Leer de un fichero todos los eventos de un año");
+        System.out.println("\t9.- Guardar de un fichero de texto todos los eventos de un año");
+        System.out.println("\t10.- Guardar en un fichero de texto todos los eventos de un mes");
+        System.out.println("\t11.- Guardar en un fichero de texto todos los eventos de un día");
+        System.out.println("\t12.- Salir");
         op = comprobarScanner(12);
         switch (op) {
             case 1: break;
@@ -97,13 +98,31 @@ public class Menu
         }
     }//opcionesEventosTareas()
     
-    private void opcionesImprimirDias()
+    private void opcionesImprimirDias() throws AWTException, InterruptedException
     {
-        
+        int op = -1;
+        limpiar();
+        System.out.println("\n\tIntroduce el Mes del que Quieres Imprimir los Dias del Calendario");
+        op = comprobarScanner(1);
     }//opcionesImprimirDias()
     
-    private void opcionesContactos()
+    private void opcionesContactos() throws AWTException, InterruptedException
     {
-        
+        int op = -1;
+        limpiar();
+        System.out.println("\n\t1.- Leer Contactos");
+        System.out.println("\t2.- Crear un Nuevo Contacto");
+        System.out.println("\t3.- Listar Todos los Contactos");
+        System.out.println("\t4.- Buscar un Contacto");
+        System.out.println("\t5.- Guardar Contactos");
+        System.out.println("\t6.- Salir");
+        op = comprobarScanner(6);
+        switch (op) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 5: break;
+        }
     }//opcionesContactos()
 }
