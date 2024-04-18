f8761b4f3f91da33062ca4a558f83a1c85bd3250
ALUMNO <ALUMNO@DESKTOP-282UC07>
2024-04-18 12:54:22 +0200

    ...

--- src/Agenda/Principal.java
+++ src/Agenda/Principal.java
@@ -1,5 +1,9 @@
 package Agenda;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 public class Principal 
 {
     public static void main(String[] args) 
@@ -13,4 +17,14 @@ public class Principal
         System.out.println("ada");
         System.out.println("me tenia qie cambiar de rama");
     }//main()
+    
+    private void limpiar() throws AWTException, InterruptedException
+    {
+        Robot limpiar = new Robot();
+        limpiar.keyPress(KeyEvent.VK_CONTROL);
+        limpiar.keyPress(KeyEvent.VK_L);
+        limpiar.keyRelease(KeyEvent.VK_CONTROL);
+        limpiar.keyRelease(KeyEvent.VK_L);
+        Thread.sleep(250);
+    }
 }//class
