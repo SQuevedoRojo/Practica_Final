3e29d8b67f547c85b638ea05469e8e05408cebf3
hp <hp@DESKTOP-928HOVP>
2024-04-29 11:12:10 +0200

    eventos

---
+++ src/Agenda/Contacto.java
@@ -0,0 +1,19 @@
+package Agenda;
+
+/**
+ * Clase contacto que se encarga de la creacion y guardar informacion de contacto
+ * @author Asier Sergio
+ * @version 1.0
+ */
+public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public Contacto(String nombre, String apellido, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+    
+}
